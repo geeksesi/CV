be64fde19f10becc53f91056f4a68b32bb973d4b
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-11-12 13:46:36 +0330

    copy basics of my react config

---
+++ src/component/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Options from "./Options";
+
+
+export default class App extends React.Component {
+    
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello World</h1>
+            </div>
+        );
+    }
+}
